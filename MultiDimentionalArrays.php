a6761a0e1d973e31e958878d1bb9c77df1441880
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-03-30 17:57:31 +0530

    Multi Dimentional Arrays
    Multi Dimentional Arrays in php

---
+++ MultiDimentionalArrays.php
@@ -0,0 +1,17 @@
+<?php
+
+$food = array("Healthy"=>array("salad","vegetables","pasta"),
+			  "Unhealthy"=>array("pizza","ice cream"));
+
+echo $food ["Healthy"][0];
+
+echo "<br>";
+
+echo $food ["Unhealthy"][1];
+
+
+
+
+
+
+?>
